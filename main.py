8c45759ae883d03e297f6094ecafdf9c445e946f
Alex Shifrin <alex.shifrin@gmail.com>
2022-05-03 16:54:22 -0700

    Add root endpoint with FastAPI

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from dataclasses import dataclass
 from datetime import date
 
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return "The server works!"
+
 @dataclass
 class Customer:
     id: int
