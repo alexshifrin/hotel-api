41af205ed36646b51a347c3b30d67f3d3ef2b34a
Alex Shifrin <alex.shifrin@gmail.com>
2022-05-04 16:01:40 -0700

    Implement POST /customer to create customer

--- hotel/operations/customers.py
+++ hotel/operations/customers.py
@@ -1,6 +1,13 @@
+from pydantic import BaseModel
+
 from hotel.db.engine import DBSession
 from hotel.db.models import DBCustomer
 
+
+class CustomerCreateData(BaseModel):
+    first_name: str
+    last_name: str
+    email_address: str
 
 def read_all_customers():
     session = DBSession()
@@ -10,4 +17,11 @@ def read_all_customers():
 def read_customer(customer_id: int):
     session = DBSession()
     customer = session.query(DBCustomer).get(customer_id)
+    return customer
+
+def create_customer(data: CustomerCreateData):
+    session = DBSession()
+    customer = DBCustomer(**data.dict())
+    session.add(customer)
+    session.commit()
     return customer
--- hotel/routers/customers.py
+++ hotel/routers/customers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from hotel.operations.customers import read_all_customers, read_customer
+from hotel.operations.customers import read_all_customers, read_customer, create_customer, CustomerCreateData
 
 
 router = APIRouter()
@@ -12,3 +12,7 @@ def api_read_all_customers():
 @router.get("/customer/{customer_id}")
 def api_read_customer(customer_id):
     return read_customer(customer_id)
+
+@router.post("/customer")
+def api_create_customer(customer: CustomerCreateData):
+    return create_customer(customer)
