95531a2c0e8321553ead43ca095f1f3a91467ce5
Alex Shifrin <alex.shifrin@gmail.com>
2022-05-04 15:23:33 -0700

    Implement GET /room/{room_id} endpoint

--- hotel/operations/rooms.py
+++ hotel/operations/rooms.py
@@ -6,3 +6,8 @@ def read_all_rooms():
     session = DBSession()
     rooms = session.query(DBRoom).all()
     return rooms
+
+def read_room(room_id: int):
+    session = DBSession()
+    room = session.query(DBRoom).get(room_id)
+    return room
